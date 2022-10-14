1f52aef7e1b5fc87dc0df212f7fb24725d90b6bc
Kandeepan22 <107128264+Kandeepan22@users.noreply.github.com>
2022-10-14 18:16:47 +0200

    Add files via upload

---
+++ tetrisSpiel/Tetris/src/game Spiel Logik und Blöcke/GameLogik.java
@@ -0,0 +1,7 @@
+package game;
+
+public class GameLogik {
+	
+		Block current, next;
+
+}
---
+++ tetrisSpiel/Tetris/src/ui alles was wir zum Zeichnen haben/GuiTest.java
@@ -0,0 +1,22 @@
+package ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GuiTest {
+	
+	
+	Gui gui = new Gui(width = 320, height = 576);
+
+	@Test
+	public void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+}
